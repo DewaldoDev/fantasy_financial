158e592dfd22c2118fe5b4d9d782215beaf38082
DewaldoDev <dewaldodev@gmail.com>
2015-12-15 20:07:38 -0500

    Added assign portfolios callback to Group

--- app/models/group.rb
+++ app/models/group.rb
@@ -9,8 +9,18 @@ class Group < ActiveRecord::Base
 	validates_datetime :end_date, after: :start_date
 
 	before_validation :set_start_date, on: [:create]
+	after_create :assign_portfolios
 
 	def set_start_date
 		self.start_date = Time.now
+	end
+
+	def assign_portfolios
+		self.users.each do |user|
+			user.portfolios.create(
+				group_id: self.id,
+				current_value: 0,
+				cash: self.starting_total)
+		end
 	end
 end

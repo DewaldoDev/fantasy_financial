4ba4b4ac44e7abcafe3fa9de26fc2e4bee7344ab
DewaldoDev <dewaldodev@gmail.com>
2015-12-16 11:08:18 -0500

    Added calculate_value method to Portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,13 @@ class Portfolio < ActiveRecord::Base
 	has_many :owned_stocks
 
 	validates :group_id, :user_id, presence: true
+
+	def calculate_value
+		self.current_value = self.stocks_value + self.cash
+		self.save
+	end
+
+	def stocks_value
+		self.owned_stocks.inject{|sum, stock| sum += stock.current_market_value}
+	end
 end
